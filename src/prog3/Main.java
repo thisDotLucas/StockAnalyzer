5af2a7b73e6a11115da93f7783b8fea4c723d319
Lucas fransman <43991152+IgorPolitruck@users.noreply.github.com>
2019-03-01 01:08:50 +0200

    deleted legacy classes

--- src/prog3/Main.java
+++ src/prog3/Main.java
@@ -1,12 +1,6 @@
 package prog3;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
-import json.AlphaVantage;
-import org.json.*;
-import json.FileHandler;
-import json.JSONUtils;
-import org.json.JSONObject;
 
 public class Main {
 
